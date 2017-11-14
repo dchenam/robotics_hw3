95ac5a5ffb15cee7448bd74b2e30336bf2bd2df4
dchenam <dnnschen39@gmail.com>
2017-11-14 17:55:59 +0800

    initial commit

---
+++ build.sh
@@ -0,0 +1,6 @@
+cd build
+cmake ..
+make
+mv cam_calibration ../
+cd ..
+./cam_calibration
---
+++ build/CMakeFiles/cam_calibration.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cam_calibration.dir/cam_calibration.cpp.o"
+  "cam_calibration.pdb"
+  "cam_calibration"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cam_calibration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
